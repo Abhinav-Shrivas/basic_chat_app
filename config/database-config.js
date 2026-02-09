bba1a7ba4b5acd5beaf1c327c65ba48d831e909f
Abhinav-Shrivas <abhinavshrivas2401@gmail.com>
2026-02-09 14:36:22 +0530

    completed the chat-app with TYPING status functionality

---
+++ config/database-config.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const connect = async()=>{
+    await mongoose.connect("mongodb://localhost/chatApp")
+}
+module.exports = connect
--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const express = require ('express');
 const http = require('http');
 const socketio = require('socket.io');
+const connect = require('./config/database-config');
+const Chat = require('./models/chat')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection',(socket)=>{
-    console.log("a user is connected");
-    socket.on('send msg', (data)=>{
-        console.log(data);
-        io.emit("message received",data);
+
+    socket.on('join_room', (data)=>{
+        socket.join(data.roomid);
+    });
+    socket.on('send_msg', async(data)=>{
+        const chat = await Chat.create({
+            content : data.msg,
+            user : data.username,
+            roomid: data.roomid
+        });
+        io.to(data.roomid).emit("message_received",data);
+    });
+    socket.on('typing',(data)=>{
+        socket.broadcast.to(data.roomid).emit('someoneTyping',data);
     })
 })
-
+app.set('view engine', 'ejs');
 app.use('/',express.static(__dirname + '/public'));
 
-server.listen(3000,()=>{
+app.get('/chat/:roomid',async(req,res)=>{
+    const chats = await Chat.find({
+        roomid : req.params.roomid
+    }).select('content user');
+    res.render('index',{
+        name : "Ab",
+        id : req.params.roomid,
+        chats : chats
+    })
+});
+
+server.listen(3000,async()=>{
     console.log("server started at port 3000");
+    await connect();
+    console.log("mongodb connected");
 })
